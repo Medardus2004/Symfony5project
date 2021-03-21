ee7bd494598d2848a478e02e2cbdbfc5eed69d3f
Medardus2004 <tobim.doe@web.de>
2021-03-21 18:25:17 +0100

    symfony-project was added

--- app/public/index.php
+++ app/public/index.php
@@ -1,42 +1,22 @@
 <?php
 
-echo '<p>Hallo Welt</p>';
-#use App\Kernel;
-#use Symfony\Component\Dotenv\Dotenv;
-#use Symfony\Component\ErrorHandler\Debug;
-#use Symfony\Component\HttpFoundation\Request;
+use App\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\HttpFoundation\Request;
 
-#require dirname(__DIR__).'/vendor/autoload.php';
+require dirname(__DIR__).'/vendor/autoload.php';
 
-#(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 
-#if ($_SERVER['APP_DEBUG']) {
-#    umask(0000);
+if ($_SERVER['APP_DEBUG']) {
+    umask(0000);
 
-#    Debug::enable();
-#}
+    Debug::enable();
+}
 
-#$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
-#$request = Request::createFromGlobals();
-#$response = $kernel->handle($request);
-#$response->send();
-#$kernel->terminate($request, $response);
-
-
-// src/Controller/LuckyController.php
-#namespace App\Controller;
-
-#use Symfony\Component\HttpFoundation\Response;
-
-#class LuckyController
-#{
-#    public function number(): Response
-#    {
- #       $number = random_int(0, 100);
-
-  #      return new Response(
-   #         '<html><body>Lucky number: '.$number.'</body></html>'
-    #    );
-    #}
-#}
-
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
